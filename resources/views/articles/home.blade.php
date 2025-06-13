f247e3ad387c24987463c84869517f692aac27fc
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-06-13 14:44:36 +0100

    fix composer

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -1,5 +1,11 @@
 @extends("layouts.app")
 @section("content")
 
-   <x-articles :articles="$articles" :show-header="true" :active-filters="$activeFilters" />
+    <x-articles
+        :articles="$articles"
+        :filter-route=" route('articles.index')"
+        :show-header="true"
+        :active-filters="$activeFilters"
+        :show-filter-by-user="true"
+    />
 @endsection
