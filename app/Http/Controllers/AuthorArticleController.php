a8b85ceeaac0f545e1442c28ef12b9de51119744
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-06-15 12:24:34 +0100

    wip

--- app/Http/Controllers/AuthorArticleController.php
+++ app/Http/Controllers/AuthorArticleController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\ArticleStatus;
 use App\Enums\OrderBy;
-use App\Models\Article;
 use App\Models\User;
-use Dflydev\DotAccessData\Data;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
-class ArticleController extends Controller
+
+class AuthorArticleController extends Controller
 {
-    public function index(Request $request)
+    public function __invoke(Request $request , string $username)
     {
-        $query = Article::query();
+
+        $user = User::query()->where('username' ,$username)->firstOrFail();
+
+        $query =  $user->articles()->newQuery();
         $activeFilters = 0;
+
+        $query->where('user_id', $user->id);
+
         if ($kw = $request->input('f-search')) {
             $query->where('title', 'like', "%{$kw}%");
             $activeFilters++;
@@ -31,33 +34,23 @@ class ArticleController extends Controller
             $query->where('published_at' , '<', $date);
         }
 
-        if ($userId = $request->input('f-author')) {
-            $activeFilters++;
-            $query->where('user_id' ,  $userId);
-        }
         if ($orderBy = $request->input('f-orderBy')) {
             $query->orderBy(OrderBy::from((int)$orderBy)->coulumn() , 'ASC');
         }
+
         $articles = $query
-            ->where('status', ArticleStatus::PUBLISHED->value)
             ->latest()
             ->paginate(10)
             ->withQueryString();
-//        dd(
-//            $articles
-//        );
-        return view('articles.home', [
+
+
+        return view('pages.author.user-articles.articles', [
             "activeFilters" => $activeFilters,
             "articles" => $articles,
             "input"=>$request->all()
         ]);
-    }
 
 
 
-    public function show(string $id, string $username = null, string $title = null)
-    {
-        $article = Article::query()->findOrFail($id);
-        return view('articles.show', compact('article'));
     }
 }
